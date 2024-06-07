a053ec5867ba70f0fde1e5858f91a02b76f8dc8d
ymirotvorenie <karoline.navarra@gmail.com>
2024-06-07 07:12:02 +0700

    add try-with-resources

--- src/main/java/io/hexlet/Application.java
+++ src/main/java/io/hexlet/Application.java
@@ -5,30 +5,27 @@ import java.sql.SQLException;
 
 public class Application {
     public static void main(String[] args) throws SQLException {
-        var connection = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
+        try( var connection = DriverManager.getConnection("jdbc:h2:mem:hexlet_test")) {
+            var query = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255));";
+            try (var statement = connection.createStatement()) {
+                statement.execute(query);
+            }
 
-        var query = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255));";
-        var statement = connection.createStatement();
-        statement.execute(query);
-        statement.close();
+            var query2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
+            try (var statement2 = connection.createStatement()) {
+                statement2.executeUpdate(query2);
+            }
 
-        var query2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-        var statement2 = connection.createStatement();
-        statement2.executeUpdate(query2);
-        statement2.close();
+            var query3 = "SELECT * FROM users";
+            try (var statement3 = connection.createStatement()) {
+                var resultSet = statement3.executeQuery(query3);
 
-        var query3 = "SELECT * FROM users";
-        var statement3 = connection.createStatement();
-        var resultSet = statement3.executeQuery(query3);
-
-        while (resultSet.next()) {
-            System.out.println(resultSet.getLong("id"));
-            System.out.println(resultSet.getString("username"));
-            System.out.println(resultSet.getString("phone"));
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getLong("id"));
+                    System.out.println(resultSet.getString("username"));
+                    System.out.println(resultSet.getString("phone"));
+                }
+            }
         }
-        statement3.close();
-
-        connection.close();
     }
-
 }
